fbe6ed5f22a34effb7d090d211643c113de3d11f
Cesar Gutierrez <cogutierrez@neuquen.gov.ar>
2021-07-11 13:31:14 -0300

    Ejercicio_3: estructura FOR y WHILE

---
+++ Ejercicios_Bloque_1/ejercicio_3.py
@@ -0,0 +1,10 @@
+print("########## Con FOR ##########")
+for numero in range(1,61):
+    print(f"Numero: {numero} y su cuadrado es: {numero*numero}")
+
+contador = 1
+print("########## Con WHILE ##########")
+while contador <= 60:
+    print(f"Numero: {contador} y su cuadrado es: {contador*contador}")
+    contador+=1
+
