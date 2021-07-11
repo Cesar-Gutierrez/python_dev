a2fb80306de7806cb69321f9a90eaa2137371b40
Cesar Gutierrez <cogutierrez@neuquen.gov.ar>
2021-07-11 13:40:09 -0300

    Ejercicio_6: estructuras FOR anidadas

---
+++ Ejercicios_Bloque_1/ejercicio_6.py
@@ -0,0 +1,4 @@
+for numero_tabla in range(1,11):
+    print( f"#### Esta es la tabla del {numero_tabla} ####")
+    for numero in range(1,11):
+        print(f"{numero_tabla} X {numero} = {numero*numero_tabla}")
